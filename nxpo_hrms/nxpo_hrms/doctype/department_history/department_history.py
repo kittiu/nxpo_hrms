7a24b2d079d00f53efab4327125bf4ea83d57d66
Kitti U <kittiu@gmail.com>
2024-04-24 19:49:16 +0700

    /

---
+++ nxpo_hrms/nxpo_hrms/doctype/department_history/department_history.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ecosoft and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class DepartmentHistory(NestedSet):
+	pass
