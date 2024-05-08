43439bd8a7be4541bdfc405d9afe821979cecaa2
Kitti U <kittiu@gmail.com>
2024-05-08 21:48:44 +0700

    ADD PVD Report

---
+++ nxpo_hrms/nxpo_hrms/report/pvd_report/pvd_report.js
@@ -0,0 +1,45 @@
+frappe.query_reports["PVD Report"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			reqd: 1,
+			default: frappe.defaults.get_user_default("Company"),
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			reqd: 1,
+            default: frappe.datetime.month_start(),
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			reqd: 1,
+            default: frappe.datetime.month_end(),
+		},
+		{
+			fieldname: "docstatus",
+			label: __("Document Status"),
+			fieldtype: "Select",
+			options: [
+                {value: 0, label: "Draft"},
+                {value: 1, label: "Submitted"},
+                {value: 2, label: "Cancelled"},
+            ],
+			default: 1,
+			reqd: 1,
+		},
+		{
+			fieldname: "pvd_type",
+			label: __("PVD Type"),
+			fieldtype: "Link",
+			options: "PVD Type",
+			reqd: 1,
+		},
+    ],
+};
