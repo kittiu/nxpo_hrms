c749b8a4f42ffbf48cc8a84cf596f866ccae9d9f
Kitti U <kittiu@gmail.com>
2024-05-28 09:17:00 +0700

    Add API Create Employee

--- nxpo_hrms/custom/employee.py
+++ nxpo_hrms/custom/employee.py
@@ -92,3 +92,34 @@ def get_employee_property_history_html(employee):
         result.append(d)
     return frappe.render_template("nxpo_hrms/custom/employee/property_history.html", {"data": data})
 
+
+@frappe.whitelist()
+def create_employee():
+    # nxpo_hrms.custom.employee.create_employee
+    data = frappe._dict(json.loads(frappe.request.data))
+    data["doctype"] = "Employee"
+    employee_dict = {
+        "doctype": "Employee",
+        "preferred_contact_email": "Company Email",
+        "name": data.name,
+        "first_name": data.first_name,
+        "last_name": data.last_name,
+        "custom_first_name_en": data.custom_first_name_en,
+        "custom_last_name_en": data.custom_last_name_en,
+        "gender": data.gender,
+        "date_of_birth": data.date_of_birth,
+        "date_of_joining": data.date_of_joining,
+        "custom_probation_days": data.probation_days,  # default to 180
+        "cell_number": data.cell_number,
+        "personal_email": data.personal_email,
+        "company_email": data.company_email,
+        "custom_house_no": data.house_no,
+        "custom_street": data.street,
+        "custom_subdistrict": data.subdistrict,
+        "custom_district": data.district,
+        "custom_province": data.province,
+        "custom_zip_code": data.zip_code,
+        "bio": data.bio
+    }
+    employee = frappe.get_doc(employee_dict)
+    return employee.insert()
