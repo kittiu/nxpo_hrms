34bc3730d51b7a26f60db8990eb13331b6bbcf5c
Kitti U <kittiu@gmail.com>
2024-06-27 21:17:28 +0700

    /

--- nxpo_hrms/custom/user.py
+++ nxpo_hrms/custom/user.py
@@ -9,7 +9,7 @@ def get_all_roles():
 		"Role",
 		filters={
 			"name": ("not in", frappe.permissions.AUTOMATIC_ROLES),
-			"name": ("not like", "WF%"),
+			"name": ("not like", "WF%"),  # Override
 			"disabled": 0,
 		},
 		or_filters={"ifnull(restrict_to_domain, '')": "", "restrict_to_domain": ("in", active_domains)},
