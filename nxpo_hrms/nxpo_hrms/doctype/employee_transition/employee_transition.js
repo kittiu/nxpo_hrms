25f3a2b363c38e9b38253f31809f0f01fae787c4
MannyjungDev <mueangman@goingjesse.com>
2024-07-17 11:56:55 +0700

    [Edited]: Designation Filter

--- nxpo_hrms/nxpo_hrms/doctype/employee_transition/employee_transition.js
+++ nxpo_hrms/nxpo_hrms/doctype/employee_transition/employee_transition.js
@@ -1,16 +1,8 @@
 // Copyright (c) 2024, Ecosoft and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Employee Transition", {
-	refresh(frm) {
-        frm.set_query("designation", function(){
-            return {
-                filters: {
-                    "disabled": 0
-                }
-            }
-        });
-	},
-
-
-});
+// frappe.ui.form.on("Employee Transition", {
+// 	refresh(frm) {
+    
+// 	},
+// });
