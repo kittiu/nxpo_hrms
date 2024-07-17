44058703d3e7481078c34e6366657e9fd66a4932
MannyjungDev <mueangman@goingjesse.com>
2024-07-17 10:51:07 +0700

    [Fixed]: #5514, #5515, #5516, #5517

--- nxpo_hrms/nxpo_hrms/doctype/employee_transition/employee_transition.js
+++ nxpo_hrms/nxpo_hrms/doctype/employee_transition/employee_transition.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, Ecosoft and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Transition", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee Transition", {
+	refresh(frm) {
+        frm.set_query("designation", function(){
+            return {
+                filters: {
+                    "disabled": 0
+                }
+            }
+        });
+	},
 
-// 	},
-// });
+
+});
