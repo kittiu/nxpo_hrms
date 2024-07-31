891d6116a3ecc82cbd0c295d7b516604ec86e36a
MannyjungDev <mueangman@goingjesse.com>
2024-07-31 17:17:34 +0700

    [Edied]:  #5554, #5523, #5544

--- nxpo_hrms/nxpo_hrms/page/my_documents/my_documents.py
+++ nxpo_hrms/nxpo_hrms/page/my_documents/my_documents.py
@@ -46,7 +46,7 @@ def get_salary_slips(date_range, company=None, field=None, employee=None, limit=
 
 	salary_slips = frappe.get_list(
 		doctype="Salary Slip",
-		fields=["name", "posting_date", select_field, "employee_name"],
+		fields=["name", "posting_date as month_period", select_field, "employee_name"],
 		filters=filters
 	)
 
@@ -70,7 +70,7 @@ def get_wht_cert_employee(date_range, company=None, field=None, employee=None, l
 
 	salary_slips = frappe.get_list(
 		doctype="Withholding Tax Cert Employee",
-		fields=["name", select_field, "employee_name"],
+		fields=["name","date as month_period", select_field, "employee_name"],
 		filters=filters
 	)
 
