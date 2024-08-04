4260a26047b373eae609b7051da85995dbb1ae79
Kitti U <kittiu@gmail.com>
2024-08-04 16:20:24 +0700

    Split Bonus Tax by Deduct Line (method 1)

--- nxpo_hrms/custom/salary_slip.py
+++ nxpo_hrms/custom/salary_slip.py
@@ -3,6 +3,63 @@
 import frappe
 from frappe import _
 from frappe.utils import date_diff
+from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
+from hrms.payroll.doctype.salary_slip.salary_slip import calculate_tax_by_tax_slab
+
+
+class SalarySlipNXPO(SalarySlip):
+
+    def calculate_variable_based_on_taxable_salary(self, tax_component):
+        tax_amount = super().calculate_variable_based_on_taxable_salary(tax_component)
+        split_tax_amount = self.calculate_split_tax(tax_component, self.total_structured_tax_amount)
+        for d in self.deductions:
+            if d.custom_split_tax_deduction_for:
+                self.remove(d)
+        # Passing values to add_tax_components()
+        frappe.flags.split_tax_amount = split_tax_amount
+        frappe.flags.split_tax_component = tax_component
+        # --
+        return tax_amount - split_tax_amount
+    
+    def add_tax_components(self):
+        super().add_tax_components()
+        if frappe.flags.split_tax_amount:
+            self.append("deductions",
+                {
+                    "salary_component": frappe.flags.split_tax_component,
+                    "amount": frappe.flags.split_tax_amount,
+                    "custom_split_tax_deduction_for": self.custom_split_tax_deduction_on,
+                }
+            )
+
+    def calculate_split_tax(self, tax_component, total_structured_tax_amount):
+        eval_locals, default_data = self.get_data_for_eval()
+        # Total taxable earnings with spliting addl earning with full tax
+        if not self.custom_split_tax_deduction_on:
+            return 0.0
+        split_tax_addl_earning = 0.0
+        for e in self.earnings:
+            # Only addional earning with full tax is eligible
+            if not (
+                e.is_tax_applicable and
+                e.additional_salary and
+                e.deduct_full_tax_on_selected_payroll_date
+            ):
+                continue
+            # Match with Split Tax Deduction On
+            if e.salary_component == self.custom_split_tax_deduction_on:
+                split_tax_addl_earning += e.amount
+        if split_tax_addl_earning:
+            earnings = (
+                self.total_taxable_earnings_without_full_tax_addl_components +
+                split_tax_addl_earning
+            )
+            split_addl_earning_tax = calculate_tax_by_tax_slab(
+                earnings, self.tax_slab, self.whitelisted_globals, eval_locals
+            )
+            # Return only tax amount on split tax earning component
+            return split_addl_earning_tax - total_structured_tax_amount
+        return 0.0
 
 
 def validate_no_salary(doc, method=None):
--- nxpo_hrms/hooks.py
+++ nxpo_hrms/hooks.py
@@ -279,6 +279,7 @@ jinja = {
 
 override_doctype_class = {
 	"Employee": "nxpo_hrms.custom.employee.EmployeeNXPO",
+	"Salary Slip": "nxpo_hrms.custom.salary_slip.SalarySlipNXPO",
 	"Salary Structure Assignment": "nxpo_hrms.custom.salary_structure_assignment.SalaryStructureAssignmentNXPO",
     "Leave Policy Assignment": "nxpo_hrms.custom.leave_policy_assignment.LeavePolicyAssignmentNXPO",
 }
