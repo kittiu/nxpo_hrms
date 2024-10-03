3652fe0148127218c5eeb48cec58acf71b65f3be
Kitti U <kittiu@gmail.com>
2024-10-03 23:15:17 +0700

    Code Review by Kitti U.

--- nxpo_hrms/nxpo_hrms/doctype/salary_certificate/salary_certificate.py
+++ nxpo_hrms/nxpo_hrms/doctype/salary_certificate/salary_certificate.py
@@ -1,36 +1,43 @@
 # Copyright (c) 2024, Ecosoft and contributors
 # For license information, please see license.txt
 
-# import frappe
 import frappe
 from frappe.model.document import Document
-from frappe.utils import getdate
 from frappe import _
-
+from frappe import share
 
 
 class SalaryCertificate(Document):
+
 	def validate(self):
-		if not self.employee:
-			frappe.throw("กรุณาเลือกพนักงาน.")
-	
-		if not self.from_date:
-			frappe.throw("กรุณาเลือก From Date.")
-                          
-		from_date = getdate(self.from_date)
-		
-		salary_structure_assignment = frappe.db.get_value(
+		self.set_salary_info()
+
+	def on_update(self):
+		self.share_to_approver()
+
+	def set_salary_info(self):
+		ssa = frappe.get_all(
             "Salary Structure Assignment",
             {
                 "employee": self.employee,
-                "from_date": ("<=", from_date)
+                "from_date": ("<=", self.date_of_certification),
+				"docstatus": 1,
             },
-            ["name", "salary_structure", "base"],  
-            as_dict=True
+            ["name", "base"],
+			order_by="from_date desc",
+			limit=1
         )
-		if not salary_structure_assignment:
-			frappe.throw("ไม่พบ Salary Structure Assignment ที่ตรงกับ From Date ที่เลือก.")
-			
-		self.salary_structure_assignment = salary_structure_assignment.get("name")
-		self.salary_structure = salary_structure_assignment.get("salary_structure")
-		self.base_salary = salary_structure_assignment.get("base")
+
+		if not ssa:
+			frappe.throw(_("Salary Structure Assignment not found for Employee {0}").format(self.employee))
+
+		self.salary_structure_assignment = ssa[0].name
+		self.base_salary = ssa[0].base
+
+	def share_to_approver(self):
+		users = [x.user for x in share.get_users(self.doctype, self.name)]
+		for user in users:
+			if user != frappe.session.user:
+				share.remove(self.doctype, self.name, user)
+		if self.approver and self.approver != frappe.session.user:
+			share.add_docshare(self.doctype, self.name, user=self.approver, read=1, submit=1, notify=1)
